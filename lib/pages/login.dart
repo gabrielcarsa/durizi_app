a9049f9ab56a60a88261e656dde051f617485732
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-04-23 13:38:07 -0400

    refactor: tirando importações não usadas

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,9 +2,7 @@ import 'package:durizi_app/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import '../models/Cliente.dart';
 import '../providers/ClienteProvider.dart';
-import '../widgets/grafico_evolucao.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
